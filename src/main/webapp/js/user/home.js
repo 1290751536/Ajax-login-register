c2205799ab83fc06a06c526b7c97a9683714af0a
86131 <1290751536@qq.com>
2021-11-04 16:25:55 +0800

    Update Logout

--- src/main/webapp/js/user/home.js
+++ src/main/webapp/js/user/home.js
@@ -1,23 +1,31 @@
 $(function () {
     updateHead();
+
+    $("#logout-btn").click(function () {
+        $.getJSON('/user/logout.do', function (msg) {
+            if (msg.success) {
+                alert('登出成功');
+                window.location.href = '/login.html';
+            }
+        })
+    })
 })
 
 
 function updateHead() {
-    $.getJSON("/user/getsession.do",function (data) {
-        if(data.success){ // 说明用户已经登录了
-            $("#login").css("display","none");
-            $("#register").css("display","none");
-            $("#exist-user-name").css("display","");
-            $("#logout").css("display","");
+    $.getJSON("/user/getsession.do", function (data) {
+        if (data.success) { // 说明用户已经登录了
+            $("#login").css("display", "none");
+            $("#register").css("display", "none");
+            $("#exist-user-name").css("display", "");
+            $("#logout").css("display", "");
 
             $("#exist-user-name").text(data.user.custNo);
-        }
-        else{ // 用户还未登录
-            $("#login").css("display","");
-            $("#register").css("display","");
-            $("#exist-user-name").css("display","none");
-            $("#logout").css("display","none");
+        } else { // 用户还未登录
+            $("#login").css("display", "");
+            $("#register").css("display", "");
+            $("#exist-user-name").css("display", "none");
+            $("#logout").css("display", "none");
         }
     })
 }
